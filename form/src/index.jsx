ac0b229b0706365721b6ed68cb472bfcd15228dd
richer.xie <richer.xie@ucloud.cn>
2025-04-18 18:51:16 +0800

    docs(form):修改了部分代码

--- form/src/index.jsx
+++ form/src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // React 18 的写法
-import MainPage from './app';  // 导入你的App组件
+import ReactDOM from 'react-dom/client'; 
+import MainPage from './app'; 
 
-// 获取根DOM元素
 const rootElement = document.getElementById('root');
 
-// 使用 ReactDOM.createRoot 渲染 App 组件
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
